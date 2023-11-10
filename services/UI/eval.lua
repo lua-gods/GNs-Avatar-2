5bcc5ac4ea47950145f953d84252ec32b9632c28
GNamimates <81744903+GNamimates@users.noreply.github.com>
2023-11-11 02:55:57 +0800

    convert

--- services/UI/eval.lua
+++ services/UI/eval.lua
@@ -11,4 +11,12 @@ command.register(function (words)
          command.announce(result)
       end
    end
+   print()
+   if words[1] == "convert" then
+      if words[3] == "in"  and #words == 4 then
+         command.announce((words[2] / words[4]) * 100 .. "% chance")
+      elseif words[2]:sub(#words[2],#words[2]) == "%" and #words == 2 then
+         command.announce(" 1 in " .. tostring(100 / tonumber(words[2]:sub(1,#words[2]-1))) .. " chance")
+      end
+   end
 end)
