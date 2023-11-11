050d9a48e6a2f323f681bdef8cd008505fa21c22
GNamimates <81744903+GNamimates@users.noreply.github.com>
2023-11-12 02:59:37 +0800

    low teir GN on lightning bolt strike

---
+++ services/UI/lmao.lua
@@ -0,0 +1,25 @@
+local anchor = require("services.UI.anchors")
+local overlay = anchor.TopLeft:newSprite("NOW"):texture(textures.cute):setRenderType("BLURRY")
+
+anchor.SCREEN_RESIZED:register(function (size)
+   local dim = overlay:getDimensions()
+   local scale = size / dim
+   overlay:scale(scale.x,scale.y,1)
+end)
+
+local visibility = 1
+
+events.WORLD_TICK:register(function ()
+   if visibility > 0 then
+      overlay:visible(true):setColor(1,1,1,math.min(visibility/20,1))
+      visibility = visibility - 1
+   else
+      overlay:visible(false)
+   end
+end)
+
+events.ON_PLAY_SOUND:register(function (id, pos, volume, pitch, loop, category, path)
+   if id == "minecraft:entity.lightning_bolt.thunder" then
+      visibility = 20
+   end
+end)
