780d2f4d5f2d36ff7feb91b12f37e85bcb1df4f6
GNamimates <81744903+GNamimates@users.noreply.github.com>
2023-11-12 02:59:55 +0800

    eval command

--- services/UI/eval.lua
+++ services/UI/eval.lua
@@ -2,9 +2,9 @@ local command = require("services.command")
 
 command.register(function (words)
    if words[1] == "eval" then
-      if words[3] == "in"  and #words == 4 then
+      if #words == 4 and words[3] == "in" then
          command.announce((words[2] / words[4]) * 100 .. "% chance")
-      elseif words[2]:sub(-1,-1) == "%" and #words == 2 then
+      elseif #words >= 2 and words[2]:sub(-1,-1) == "%" and #words == 2 then
          command.announce(" 1 in " .. 100 / tonumber(words[2]:sub(1,-2)) .. " chance")
       else
          local expression = ""
