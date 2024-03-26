fda871d31e69ecae60e122a5dacff70107b7f6bd
GNamimates <81744903+GNamimates@users.noreply.github.com>
2024-03-27 01:38:29 +0800

    disabled chat blocking

--- programs/chat.lua
+++ programs/chat.lua
@@ -6,12 +6,12 @@ local new_message = 0
 local parseChat = require("libraries.chatHandler")
 events.CHAT_RECEIVE_MESSAGE:register(function (message, json_text)
    local json = parseJson(json_text)
-   if json and json.translate == "chat.type.text" then
-      local block = false
-      if mute[json.with[1].text] then
-         new_message = new_message + 1
-         return nil
-      end
-   end
+   --if json and json.translate == "chat.type.text" then
+   --   local block = false
+   --   if mute[json.with[1].text] then
+   --      new_message = new_message + 1
+   --      return nil
+   --   end
+   --end
    return parseChat(json_text)
 end)
