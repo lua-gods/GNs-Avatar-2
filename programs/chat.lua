e419319b3a2ed234fa6d4f830fdf91b353393665
GNamimates <81744903+GNamimates@users.noreply.github.com>
2024-04-06 00:13:46 +0800

    added proof of concept for filtering

--- programs/chat.lua
+++ programs/chat.lua
@@ -2,5 +2,21 @@ if not host:isHost() then return end
 
 local parseChat = require("libraries.chatFormatter")
 events.CHAT_RECEIVE_MESSAGE:register(function (message, json_text)
+   if message == "Could not set the block" then
+      return false
+   end
    return parseChat(json_text)
 end)
+
+--local filters = {
+--   what = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+--   with = "48CD3F6H1JKLMN0PQR5+UVWXY2",
+--}
+--
+--events.CHAT_SEND_MESSAGE:register(function (message)
+--   for i = 1, #filters.what, 1 do
+--      local a,b = filters.what:sub(i,i), filters.with:sub(i,i)
+--      message = message:upper():gsub(a,b)
+--   end
+--   return message:lower()
+--end)
