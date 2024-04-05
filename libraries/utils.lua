7d1f0268f26dda72a0754955f44d0fce3012e085
GNamimates <81744903+GNamimates@users.noreply.github.com>
2024-04-06 03:46:08 +0800

    I thought I already commited this

--- libraries/utils.lua
+++ libraries/utils.lua
@@ -119,14 +119,16 @@ local t = {}
 ---Creates a proxy table for the given table, the proxy table is read only.  
 ---2nd parameter is a metatable.
 ---@param tbl table
----@param metatbl table?
 ---@return table
-function utils.makeTableReadOnly(tbl,metatbl)
+function utils.makeTableReadOnly(tbl)
    local proxy = {}
    local mt = {
-   __index = metatbl ~= nil and (metatbl or tbl) or tbl,
+   __index = tbl,
+   __metatable = function ()
+      error("Attempt to modify read-only table", 2)
+   end,
    __newindex = function ()
-   error("No modifying metatable for u :trol:", 2)
+   error("Attempt to modify read-only table", 2)
    end
    }
    setmetatable(proxy, mt)
